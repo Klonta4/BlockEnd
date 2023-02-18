be52f3ce161794113d407e280c66686ac2977296
Alexandr Voronin <sasha-voronin2001@mail.ru>
2023-02-18 04:15:35 +0300

    all function worked

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Globalization;
+
+int counter = 0;
+int k = 0;
+int lenght;
+int j;
+
+Console.Write("Enter lenght massive: ");
+lenght = int.Parse(Console.ReadLine());
+
+string[] mass = new string[lenght];
+
+while (lenght > k)
+{
+    Console.WriteLine("enter " + k + " element of massive");
+    mass[k] = Console.ReadLine();
+    k++;
+}
+
+for (int i = 0; mass.Count() > i; i++)
+{
+    if (mass [i].Length <= 3)
+    {
+        counter++;
+    }
+}
+Console.WriteLine("Count elements lower then 3: " + counter);
